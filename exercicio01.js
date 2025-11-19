d777e57d9a71711bcb7cd247b01e13aa821323f7
RogerAraujo-cyber <00001124782977sp@al.educacao.sp.gov.br>
2025-11-19 14:25:50 -0300

    Add files via upload

---
+++ exercicio01.js
@@ -0,0 +1,10 @@
+let username = prompt("Digite o seu usuário: ")
+
+let senha = prompt("Digite a sua senha: ")
+
+if(username == "admin"  && senha == "senha123"){
+console. log("Login bem-sucedido!")
+}
+else{
+    console. log("Login incorreto")
+}
---
+++ exercicio02.js
@@ -0,0 +1,36 @@
+let dia_semana = prompt("Informe um número de 1 a 7: ")
+
+switch (dia_semana) {
+    case "1":
+        console.log("Segunda-feira")
+        break;
+
+    case "2":
+        console.log("Terça-feira")
+        break;
+
+    case "3":
+        console.log("Quarta-feira")
+        break;
+
+    case "4":
+        console.log("Quinta-feira")
+        break;
+
+    case "5":
+        console.log("Sexta-feira")
+        break;
+
+    case "6":
+        console.log("Sabado")
+        break;
+
+    case "7":
+        console.log("Domingo")
+        break;
+
+
+    default:
+        console.log("O número informado não é valido")
+        break;
+}
